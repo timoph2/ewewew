ef9a7733934a941ae2295217c494bb070f72ae00
timoph2 <98256868+timoph2@users.noreply.github.com>
2023-06-04 11:16:10 +0800

    Add files via upload

---
+++ module/ec2_mod.tf
@@ -0,0 +1,7 @@
+
+resource "aws_instance" "ec2_d" {
+    ami = "ami-0715c1897453cabd1"
+    # instance_type = "t2.micro"           #hardcoding this instance type like this is *final* value
+    instance_type = var.instance_type      #leaving it as variable default, allow to be overwritten
+    security_groups = ["default"]
+}
---
+++ module/output.tf
@@ -0,0 +1,5 @@
+output "my_ec2_id_output" {
+  value = aws_instance.ec2_d.id
+}
+
+
